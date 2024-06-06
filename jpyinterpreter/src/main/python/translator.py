2807e21ef92fb3db15cb08f8219a0a491025c33a
Christopher Chianelli <christopher@timefold.ai>
2024-06-06 09:47:51 -0400

    fix: Copy inspect code for getmembers_static
    - Note: Python Software Foundation License is similar to
      the Apache license; https://www.apache.org/legal/resolved.html#category-a

--- jpyinterpreter/src/main/python/translator.py
+++ jpyinterpreter/src/main/python/translator.py
@@ -512,14 +512,52 @@ def get_members_static(object, predicate):
     try:
         return inspect.getmembers_static(object, predicate)
     except AttributeError:
-        first_pass = inspect.getmembers(object, predicate)
-        out = []
-        for key, value in first_pass:
-            if key in object.__dict__:
-                out.append((key, object.__dict__[key]))
+        return _getmembers(object, predicate, type.__getattribute__)
+
+
+# TODO: Remove me when minimum Python version is 3.11
+def _getmembers(object, predicate, getter):
+    import types
+    results = []
+    processed = set()
+    names = dir(object)
+    if inspect.isclass(object):
+        mro = (object,) + inspect.getmro(object)
+        # add any DynamicClassAttributes to the list of names if object is a class;
+        # this may result in duplicate entries if, for example, a virtual
+        # attribute with the same name as a DynamicClassAttribute exists
+        try:
+            for base in object.__bases__:
+                for k, v in base.__dict__.items():
+                    if isinstance(v, types.DynamicClassAttribute):
+                        names.append(k)
+        except AttributeError:
+            pass
+    else:
+        mro = ()
+    for key in names:
+        # First try to get the value via getattr.  Some descriptors don't
+        # like calling their __get__ (see bug #1785), so fall back to
+        # looking in the __dict__.
+        try:
+            value = getter(object, key)
+            # handle the duplicate key
+            if key in processed:
+                raise AttributeError
+        except AttributeError:
+            for base in mro:
+                if key in base.__dict__:
+                    value = base.__dict__[key]
+                    break
             else:
-                out.append((key, value))
-        return out
+                # could be a (currently) missing slot member, or a buggy
+                # __dir__; discard and move on
+                continue
+        if not predicate or predicate(value):
+            results.append((key, value))
+        processed.add(key)
+    results.sort(key=lambda pair: pair[0])
+    return results
 
 
 def translate_python_class_to_java_class(python_class):
